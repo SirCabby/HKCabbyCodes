596db95ce0a89f9ea8404b6f4ce79eaa5fb4a6f0
SirCabby <77202713+SirCabby@users.noreply.github.com>
2023-10-31 14:24:47 -0400

    init basic menu

---
+++ CabbyCodes/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Class Declaration", "BepInEx002:Classes with BepInPlugin attribute must inherit from BaseUnityPlugin", Justification = "<Pending>", Scope = "type", Target = "~T:CabbyCodes.CabbyCodesPlugin")]
---
+++ CabbyCodes/ToggleButton.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CabbyCodes
+{
+    public class ToggleButton
+    {
+        private readonly GameObject toggleButton;
+        private readonly Color onColor = new Color(0, 0.8f, 1, 1);
+        private readonly Color offColor = Color.white;
+        public bool isOn { get; set; }
+
+        public ToggleButton()
+        {
+            toggleButton = DefaultControls.CreateButton(new DefaultControls.Resources());
+            toggleButton.name = "Toggle Button";
+            toggleButton.GetComponent<Button>().onClick.AddListener(Toggle);
+
+            Text toggleText = toggleButton.GetComponentInChildren<Text>();
+            toggleText.fontSize = 36;
+            Update();
+        }
+
+        public GameObject GetGameObject()
+        {
+            return toggleButton;
+        }
+
+        public void Toggle()
+        {
+            isOn = !isOn;
+            Update();
+        }
+
+        private void Update()
+        {
+            Text toggleText = toggleButton.GetComponentInChildren<Text>();
+            Image toggleImage = toggleButton.GetComponent<Image>();
+            if (isOn)
+            {
+                toggleText.text = "ON";
+                toggleImage.color = onColor;
+            }
+            else
+            {
+                toggleText.text = "OFF";
+                toggleImage.color = offColor;
+            }
+        }
+    }
+}
