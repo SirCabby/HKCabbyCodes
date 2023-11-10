89b7affb5288ba3b6a92687a6368ecd4999846e2
SirCabby <77202713+SirCabby@users.noreply.github.com>
2023-11-10 00:56:34 -0500

    Create PanelAdder.cs

---
+++ CabbyCodes/UI/CheatPanels/PanelAdder.cs
@@ -0,0 +1,40 @@
+﻿using CabbyCodes.UI.Factories;
+using CabbyCodes.UI.Modders;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+namespace CabbyCodes.UI.CheatPanels
+{
+    public class PanelAdder
+    {
+        public static GameObject AddButton(CheatPanel panel, int siblingIndex, UnityAction action, string buttonText, Vector2 size)
+        {
+            GameObject buttonPanel = DefaultControls.CreatePanel(new DefaultControls.Resources());
+            buttonPanel.name = "Button Panel";
+            new ImageMod(buttonPanel.GetComponent<Image>()).SetColor(Color.clear);
+            new Fitter(buttonPanel).Attach(panel.cheatPanel);
+            buttonPanel.transform.SetSiblingIndex(siblingIndex);
+
+            var buttonPanelLayout = buttonPanel.AddComponent<LayoutElement>();
+            buttonPanelLayout.flexibleHeight = 1;
+            buttonPanelLayout.minWidth = size.x;
+
+            (GameObject button, _, _) = ButtonFactory.Build(buttonText);
+            button.GetComponent<Button>().onClick.AddListener(action);
+            new Fitter(button).Attach(buttonPanel).Anchor(new Vector2(0.5f, 0.5f), new Vector2(0.5f, 0.5f)).Size(size);
+
+            return buttonPanel;
+        }
+
+        public static GameObject AddDestroyPanelButton(CheatPanel panel, int siblingIndex, Action additionalAction, string buttonText, Vector2 size)
+        {
+            return AddButton(panel, siblingIndex, delegate
+            {
+                UnityEngine.Object.Destroy(panel.cheatPanel);
+                additionalAction();
+            }, buttonText, size);
+        }
+    }
+}
