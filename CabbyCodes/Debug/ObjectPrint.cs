de50ac0ea4ba5fceaa03faad5dddbfb4f1d414aa
SirCabby <77202713+SirCabby@users.noreply.github.com>
2023-11-09 23:34:34 -0500

    expanded debug prints

--- CabbyCodes/Debug/ObjectPrint.cs
+++ CabbyCodes/Debug/ObjectPrint.cs
@@ -7,15 +7,40 @@ namespace CabbyCodes.Debug
     {
         private const string tab = "    ";
 
-        public static void DisplayObjectInfo(Object o)
+        public static void DisplayObjectInfo(object o)
         {
-            // Include the type of the object
             Type type = o.GetType();
             CabbyCodesPlugin.BLogger.LogInfo("Type: " + type.Name);
 
-            // Include information for each Field
-            CabbyCodesPlugin.BLogger.LogInfo(tab + "Fields:");
-            FieldInfo[] fields = type.GetFields();
+            // Print each Field
+            CabbyCodesPlugin.BLogger.LogInfo(tab + "Public Fields:");
+            PrintFields(o, type.GetFields());
+
+            CabbyCodesPlugin.BLogger.LogInfo(tab + "Public Static Fields:");
+            PrintFields(o, type.GetFields(BindingFlags.Public | BindingFlags.Static));
+
+            CabbyCodesPlugin.BLogger.LogInfo(tab + "Private Fields:");
+            PrintFields(o, type.GetFields(BindingFlags.NonPublic | BindingFlags.Instance));
+
+            CabbyCodesPlugin.BLogger.LogInfo(tab + "Private Static Fields:");
+            PrintFields(o, type.GetFields(BindingFlags.NonPublic | BindingFlags.Static));
+
+            // Print each Property
+            CabbyCodesPlugin.BLogger.LogInfo(tab + "Public Properties:");
+            PrintProperties(o, type.GetProperties());
+
+            CabbyCodesPlugin.BLogger.LogInfo(tab + "Public Static Properties:");
+            PrintProperties(o, type.GetProperties(BindingFlags.Public | BindingFlags.Static));
+
+            CabbyCodesPlugin.BLogger.LogInfo(tab + "Private Properties:");
+            PrintProperties(o, type.GetProperties(BindingFlags.NonPublic | BindingFlags.Instance));
+
+            CabbyCodesPlugin.BLogger.LogInfo(tab + "Private Static Properties:");
+            PrintProperties(o, type.GetProperties(BindingFlags.NonPublic | BindingFlags.Static));
+        }
+
+        private static void PrintFields(object o, FieldInfo[] fields)
+        {
             if (fields.Length > 0)
             {
                 foreach (FieldInfo f in fields)
@@ -27,10 +52,10 @@ namespace CabbyCodes.Debug
             {
                 CabbyCodesPlugin.BLogger.LogInfo(tab + tab + "None");
             }
+        }
 
-            // Include information for each Property
-            CabbyCodesPlugin.BLogger.LogInfo(tab + "Properties:");
-            PropertyInfo[] properties = type.GetProperties();
+        private static void PrintProperties(object o, PropertyInfo[] properties)
+        {
             if (properties.Length > 0)
             {
                 foreach (PropertyInfo p in properties)
