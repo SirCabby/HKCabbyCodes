7ef70702671446bc58fdaa41c6c36061927b68d4
SirCabby <77202713+SirCabby@users.noreply.github.com>
2023-11-09 23:35:05 -0500

    removed unnecessary method

--- CabbyCodes/CodeState.cs
+++ CabbyCodes/CodeState.cs
@@ -7,12 +7,6 @@ namespace CabbyCodes
     {
         private static readonly Dictionary<string, BoxedReference> boxes = new();
 
-        public CodeState Add(string key, BoxedReference boxedReference)
-        {
-            boxes.Add(key, boxedReference);
-            return this;
-        }
-
         public static BoxedReference Get(string key, object initialValue)
         {
             if (!boxes.ContainsKey(key))
