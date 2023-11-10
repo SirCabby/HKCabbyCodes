de94369fad56c47b8dc3c676326b531bad7e7f98
SirCabby <77202713+SirCabby@users.noreply.github.com>
2023-11-09 23:43:27 -0500

    unused usings

--- CabbyCodes/Patches/TeleportPatch.cs
+++ CabbyCodes/Patches/TeleportPatch.cs
@@ -7,8 +7,6 @@ using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 using BepInEx.Configuration;
-using CabbyCodes.Debug;
-using static GamepadVibrationMixer.GamepadVibrationEmission;
 
 namespace CabbyCodes.Patches
 {
