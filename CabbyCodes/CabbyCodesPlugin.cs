c4e908222056d70372e72ff024bb79ff688575ef
SirCabby <77202713+SirCabby@users.noreply.github.com>
2023-11-13 21:17:18 -0500

    Add GeoValuePatch

--- CabbyCodes/CabbyCodesPlugin.cs
+++ CabbyCodes/CabbyCodesPlugin.cs
@@ -29,13 +29,14 @@ namespace CabbyCodes
             cabbyMenu.AddCheatPanel(new TogglePanel(new SoulPatch(), "Infinite Soul"));
             cabbyMenu.AddCheatPanel(new TogglePanel(new GeoPatch(), "Infinite Geo"));
             cabbyMenu.AddCheatPanel(new TogglePanel(new PermadeathPatch(), "Steel Soul Mode"));
+            GeoValuePatch.AddPanel();
         }
 
         private void BuildTeleportCheats()
         {
             cabbyMenu.AddCheatPanel(new InfoPanel("Teleportation: Select common point of interest to travel to").SetColor(CheatPanel.headerColor));
             cabbyMenu.AddCheatPanel(new InfoPanel("Warning: Teleporting requires a pause / unpause to complete").SetColor(CheatPanel.warningColor));
-            cabbyMenu.AddCheatPanel(new DropdownPanel(new TeleportPatch(), "Select Area to Teleport"));
+            cabbyMenu.AddCheatPanel(new DropdownPanel(new TeleportPatch(), 400, "Select Area to Teleport"));
             cabbyMenu.AddCheatPanel(new InfoPanel("Lloyd's Beacon: Save and recall custom teleportation locations").SetColor(CheatPanel.headerColor));
             cabbyMenu.AddCheatPanel(new ButtonPanel(TeleportPatch.SaveTeleportLocation, "Save", "Save a custom teleport at current position"));
             foreach (TeleportLocation location in TeleportPatch.savedTeleports)
---
+++ CabbyCodes/Patches/GeoValuePatch.cs
@@ -0,0 +1,28 @@
+﻿using CabbyCodes.SyncedReferences;
+using CabbyCodes.UI.CheatPanels;
+using CabbyCodes.UI;
+using System;
+
+namespace CabbyCodes.Patches
+{
+    public class GeoValuePatch : ISyncedReference<int>
+    {
+        public int Get()
+        {
+            return PlayerData.instance.geo;
+        }
+
+        public void Set(int value)
+        {
+            value = Math.Max(0, value);
+            value = Math.Min(9999999, value);
+            PlayerData.instance.geo = value;
+        }
+
+        public static void AddPanel()
+        {
+            InputFieldPanel<int> panel = new(new GeoValuePatch(), KeyCodeMap.ValidChars.Numeric, 9, 200, "Edit Current Geo (0-9999999)");
+            CabbyCodesPlugin.cabbyMenu.AddCheatPanel(panel);
+        }
+    }
+}
