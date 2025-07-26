e68ff43a23c028953f3694d706da74c953f779fb
SirCabby <77202713+SirCabby@users.noreply.github.com>
2025-07-25 23:30:04 -0400

    add room exclusions for maproompatch

--- CabbyCodes/Patches/Maps/MapRoomPatch.cs
+++ CabbyCodes/Patches/Maps/MapRoomPatch.cs
@@ -1,6 +1,7 @@
 using CabbyMenu.SyncedReferences;
 using CabbyMenu.UI.CheatPanels;
 using System.Collections.Generic;
+using System.Linq;
 using static CabbyCodes.Scenes.SceneManagement;
 using CabbyCodes.Flags;
 using CabbyCodes.SavedGames;
@@ -9,7 +10,15 @@ namespace CabbyCodes.Patches.Maps
 {
     public class MapRoomPatch : ISyncedReference<bool>
     {
-        public static readonly Dictionary<string, List<string>> roomsInMaps = GetAreaToScenesMapping();
+        /// <summary>
+        /// Scene names that should be excluded from map room patch functionality.
+        /// These scenes will not appear in the map room toggle panels.
+        /// </summary>
+        private static readonly string[] ExcludedMapScenes = {
+            "Room_temple"  // Temple of the Black Egg - should not be mappable
+        };
+
+        public static readonly Dictionary<string, List<string>> roomsInMaps = GetFilteredAreaToScenesMapping();
 
         private readonly string roomName;
 
@@ -36,6 +45,30 @@ namespace CabbyCodes.Patches.Maps
                 // Reload to refresh map ui
                 GameReloadManager.SaveAndReload();
             }
+        }
+
+        /// <summary>
+        /// Gets a filtered dictionary mapping area names to lists of scene names in that area.
+        /// Excludes scenes that are in the ExcludedMapScenes array.
+        /// </summary>
+        /// <returns>A dictionary where keys are area names and values are lists of scene names.</returns>
+        private static Dictionary<string, List<string>> GetFilteredAreaToScenesMapping()
+        {
+            var unfilteredMapping = GetAreaToScenesMapping();
+            var filteredMapping = new Dictionary<string, List<string>>();
+
+            foreach (var kvp in unfilteredMapping)
+            {
+                string areaName = kvp.Key;
+                var filteredScenes = kvp.Value.Where(sceneName => !ExcludedMapScenes.Contains(sceneName)).ToList();
+                
+                if (filteredScenes.Count > 0)
+                {
+                    filteredMapping[areaName] = filteredScenes;
+                }
+            }
+
+            return filteredMapping;
         }
 
         public static void AddPanels()
