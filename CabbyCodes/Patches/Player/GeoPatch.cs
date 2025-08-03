0f7f86e3ccba9454d6ef107a0ce34228214f96ca
SirCabby <77202713+SirCabby@users.noreply.github.com>
2025-08-03 19:58:56 -0400

    fix infinite geo patch

--- CabbyCodes/Patches/Player/GeoPatch.cs
+++ CabbyCodes/Patches/Player/GeoPatch.cs
@@ -10,7 +10,8 @@ namespace CabbyCodes.Patches.Player
         public const string key = "Geo_Patch";
         private static readonly BoxedReference<bool> value = CodeState.Get(key, false);
         private static readonly Harmony harmony = new Harmony(key);
-        private static readonly MethodInfo mOriginal = AccessTools.Method(typeof(PlayerData), nameof(PlayerData.TakeGeo));
+        private static readonly MethodInfo mOriginal1 = AccessTools.Method(typeof(PlayerData), nameof(PlayerData.TakeGeo));
+        private static readonly MethodInfo mOriginal2 = AccessTools.Method(typeof(GeoCounter), nameof(GeoCounter.TakeGeo));
 
         public bool Get()
         {
@@ -23,7 +24,8 @@ namespace CabbyCodes.Patches.Player
 
             if (Get())
             {
-                harmony.Patch(mOriginal, prefix: new HarmonyMethod(typeof(CommonPatches).GetMethod("Prefix_SkipOriginal")));
+                harmony.Patch(mOriginal1, prefix: new HarmonyMethod(typeof(CommonPatches).GetMethod("Prefix_SkipOriginal")));
+                harmony.Patch(mOriginal2, prefix: new HarmonyMethod(typeof(CommonPatches).GetMethod("Prefix_SkipOriginal")));
             }
             else
             {
