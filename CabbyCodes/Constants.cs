cc3cab66b20cfde455bec54cb1710054e72da1f6
SirCabby <77202713+SirCabby@users.noreply.github.com>
2025-09-09 11:53:53 -0400

    rev 1.1.2

--- CabbyCodes/Constants.cs
+++ CabbyCodes/Constants.cs
@@ -8,7 +8,7 @@ namespace CabbyCodes
         // Plugin Information
         public const string GUID = "cabby.cabbycodes";
         public const string NAME = "Cabby Codes";
-        public const string VERSION = "1.1.1";
+        public const string VERSION = "1.1.2";
 
         // Game Limits
         public const int MIN_HEALTH = 5;
